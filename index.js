d18bd2b91e361998f2cfa1adb81ecf529747db33
louis-ngugi <louisngugi858@gmail.com>
2025-01-03 14:18:44 +0300

    update function-lab assignment

--- index.js
+++ index.js
@@ -1,51 +1,32 @@
-// Code your solution in this file!
-function distanceFromHqInBlocks(_value) {
-    return Math.abs(_value-42 );
+// Calculates distance from HQ in blocks
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42); // HQ is located at 42nd street
 }
 
-//distanceFromHqInBlocks
-
-console.log(distanceFromHqInBlocks(43))
-
-console.log(distanceFromHqInBlocks(50))
-
-console.log(distanceFromHqInBlocks(34))
-
-// distanceFromHqInFeet
-
-function distanceFromHqInFeet(_value) {
-    return distanceFromHqInBlocks(_value) *264;
+// Calculates distance from HQ in feet
+function distanceFromHqInFeet(block) {
+  const feetPerBlock = 264;
+  return distanceFromHqInBlocks(block) * feetPerBlock;
 }
 
-console.log(distanceFromHqInFeet(43))
-
-console.log(distanceFromHqInFeet(50))
-
-console.log(distanceFromHqInFeet(34))
-
-// distanceTravelledInFeet
-
-function distanceTravelledInFeet( num1, num2) {
-    return Math.abs(num2 -num1) *264;
+// Calculates distance traveled in feet between two points
+function distanceTravelledInFeet(start, destination) {
+  const feetPerBlock = 264;
+  return Math.abs(start - destination) * feetPerBlock;
 }
 
-console.log(distanceTravelledInFeet(43, 48));
-
-console.log(distanceTravelledInFeet(50, 60));
-
-console.log(distanceTravelledInFeet(34, 28));
-
+// Calculates the fare price for the journey
 function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-    
-    if (distance <= 400) {
-      return 0; // Free for the first 400 feet
-    } else if (distance > 400 && distance <= 2000) {
-      return (distance - 400) * 0.02; // 2 cents per foot for distances > 400 and <= 2000
-    } else if (distance > 2000 && distance <= 2500) {
-      return 25; // Flat fee of $25 for distances > 2000 and <= 2500
-    } else {
-      return "cannot travel that far"; // For distances > 2500 feet
-    }
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0; // Free for the first 400 feet
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; // 2 cents per foot for distances > 400 and <= 2000
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; // Flat fee of $25 for distances > 2000 and <= 2500
+  } else {
+    return "cannot travel that far"; // For distances > 2500 feet
   }
+}
   
